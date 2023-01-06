a30cf50cd24027662e71de4b68f71f83a021aeb9
hotelarabella <muhammadrakan150@gmail.com>
2023-01-06 11:38:42 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let text1 = document.querySelector(".text1");
+let text2 = document.querySelector(".text2");
+let text3 = document.querySelector(".text3");
+let navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", function () {
+  let value = Math.round(this.window.scrollY);
+  text1.style.translate = value * 3.5 + "px";
+  text2.style.translate = value * -2.7 + "px";
+  text3.style.translate = value * 2.5 + "px";
+  // myFunction();
+});
+
+const myFunction = () => {
+  if (document.documentElement.scrollTop > 338) {
+    navbar.style.color = black;
+  }
+};
